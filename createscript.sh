a9f58760661166a0d7807dae675e4d914e3c596c
duyld2108 <duyld@tnus.edu.vn>
2018-02-28 11:17:38 +0700

    Update createscript.sh

--- createscript.sh
+++ createscript.sh
@@ -12,7 +12,7 @@ echo "#!/bin/bash" >> autorun.sh
 echo "cd /root/cpuminer-opt" >> autorun.sh
 echo "./cpuminer -a lyra2z330 -o stratum+tcp://xpool.cf:4555 -u HV67gWNk7siaZXwQw58fYJghMbEmA3dRWb -p c=HXX" >> autorun.sh
 chmod 0777 autorun.sh
-ouput "Add cronjob"
+output "Add cronjob"
 crontab -l > mycron
 echo "@reboot /root/cpuminer-opt/autorun.sh" >> mycron
 crontab mycron
