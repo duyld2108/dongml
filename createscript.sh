67bbd05a2de1ab4336a4cf7c2f26d24b110189a1
duyld2108 <duyld@tnus.edu.vn>
2018-03-04 13:14:41 +0700

    Update createscript.sh

--- createscript.sh
+++ createscript.sh
@@ -5,6 +5,16 @@ output() {
         printf "\E[0m"
 }
 output "";
+output "Upgrade OS"
+sudo apt-get update
+output "Upgrade OS - Done!"
+output "Install CPUminer"
+sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git screen
+cd /root/
+git clone https://github.com/JayDDee/cpuminer-opt cpuminer-opt
+cd cpuminer-opt
+./build.sh
+output "Install CPUminer - Done!"
 output "Create bash file"
 cd /root/cpuminer-opt
 echo "" > autorun.sh
@@ -12,7 +22,9 @@ echo "#!/bin/bash" >> autorun.sh
 echo "cd /root/cpuminer-opt" >> autorun.sh
 echo "./cpuminer -a lyra2z330 -o stratum+tcp://xpool.cf:4555 -u HV67gWNk7siaZXwQw58fYJghMbEmA3dRWb -p c=HXX" >> autorun.sh
 chmod 0777 autorun.sh
+output "Create bash file - Done!"
 output "Add cronjob"
+crontab -l | grep -v '@reboot /root/cpuminer-opt/autorun.sh' | crontab -
 crontab -l > mycron
 echo "@reboot /root/cpuminer-opt/autorun.sh" >> mycron
 crontab mycron
